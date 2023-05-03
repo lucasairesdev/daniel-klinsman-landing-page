119227f9e81e7023d9b6109ce98b4168331af326
Lucas Aires <116677841+lucasairesdev@users.noreply.github.com>
2023-05-03 12:46:13 -0300

    Adição de imagens e responsividade

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,117 +1,171 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import YoutubeVideo from '@/components/player'
+import { useEffect, useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [screenHeight, setScreenHeight] = useState(0);
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    if (window) {
+      setWidth(window.innerWidth);
+      setScreenHeight(window.innerHeight);
+    }
+  }, []);
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className='bg-black'>
+      <div className='flex justify-center items-center'>
+        <h1 className='flex text-red-600 text-9xl p-4'>ZERO</h1>
+        <h2 className='absolute text-white text-5xl'>SAINDO DO</h2>
+      </div>
+      
+      <div className='flex justify-center items-center flex-col font-bold mb-8'>
+        <p>Construa sua liberdade financeira e geográfica!</p>
+        <p>Única coisa que cai do céu é chuva</p>
+      </div>
+
+      <div className='bg-black flex justify-center items-center'>
+        <YoutubeVideo width={width} />
+      </div>
+
+      <div className='flex justify-center items-center flex-col font-bold m-4 p-4'>
+        <p>Vagas limitadas, depois que acabarem não venham</p>
+        <p>dizer que eu não avisei! CLICK no link ABAIXO!</p>
+      </div>
+
+      <div className="flex w-full items-center animate-bounce mt-4 justify-center">
+        <button className="bg-orange-500 text-black mx-auto rounded-full px-2 py-1 text-xl font-bold p-4 m-4 ">
+          Clique Aqui
+        </button>
+      </div>
+
+
+      <div className='flex justify-center items-center flex-col font-bold m-2 p-2'>
+        <p>Depoimento dos meus alunos</p>
+      </div>
+      <div className='flex w-full justify-center items-center'>
+        <div className='flex mb-8 overflow-x-auto w-[600px] gap-5'>
+          <Image
+            src="/whatsapp1.jpg"
+            height={400}
+            width={400}
+            alt='whatsapp-image1'
+          />
+
+          <Image
+            src="/whatsapp2.jpg"
+            height={400}
+            width={400}
+            alt='whatsapp-image2'
+          />
+
+          <Image
+            src="/whatsapp2.jpg"
+            height={400}
+            width={400}
+            alt='whatsapp-image2'
+          />
+          <Image
+            src="/whatsapp2.jpg"
+            height={400}
+            width={400}
+            alt='whatsapp-image2'
+          />
+          <Image
+            src="/whatsapp2.jpg"
+            height={400}
+            width={400}
+            alt='whatsapp-image2'
+          />
+          <Image
+            src="/whatsapp2.jpg"
+            height={400}
+            width={400}
+            alt='whatsapp-image2'
+          />
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      
+      <div className='flex justify-center items-center'>
+      <Image
+            src="/apresentacao-daniel.jpg"
+            height={400}
+            width={400}
+            alt='whatsapp-image2'
+          />
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <div className='flex justify-center items-center flex-col font-bold mb-8 mt-1 text-yellow-500 text-xl'>
+        <p>Prazer, Daniel Klinsmann</p>
+      </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+      {/* <div className='flex justify-center items-center flex-col font-bold'> */}
+      <div className='flex flex-col gap-5 justify-center items-center m-4 font-bold'>
+        <span className='flex items-center justify-center md:w-2/4'>
+          <p>
+            Olá! Meu nome é Daniel, trabalho com o marketing digital. Desde que comecei a
+            estudar o assunto, há cerca de 4 anos, sempre gostei muito da ideia de ser meu
+            próprio chefe e não ter que trabalhar sempre no mesmo lugar.
           </p>
-        </a>
+        </span>
+        <span className='flex items-center justify-center md:w-2/4'>
+          <p>
+            Investi muito tempo para aprender e aprimorar minhas habilidades em marketing
+            digital. Foi um processo desafiador, mas muito gratificante, que me me permitiu
+            descobrir os segredos e mistérios desse universo.
+          </p>
+        </span>
+        <span className='flex items-center justify-center md:w-2/4'>
+          <p>
+            Hoje, sinto que tenho um bom conhecimento para compartilhar e pretendo.
+            ajudar outras pessoas a entender e utilizar o marketing digital em suas vidas
+            e negócios. Por isso, quero repassar a informação da forma mais barata possível,
+            para que todos possam ter acesso a esse conhecimento valioso.
+          </p>
+        </span>
+      </div>
+      <div className='flex justify-center items-center bg-white text-black text-xl font-bold'>
+        <h5>FAQ-PERGUNTAS FREQUENTES</h5>
+      </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+      <div className='flex justify-center items-center bg-white text-black text-xl font-bold'>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+        <ul>
+          <li className='mt-4 mb-6'>1- Quais as formas de pagamento?</li>
+          <li className='mb-6'>2- Como terei acesso as aulas?</li>
+          <li className='mb-6'>3- Como posso tirar minhas dúvidas?</li>
+        </ul>
+      </div>
+
+      <div className='flex flex-col gap-5 justify-center items-center m-4 font-bold'>
+        <span className=''>
+          <p>
+            Saindo de zero é um curso que tem como objetivo  ajudar pessoas a obterem resultados financeiros utilizando o
+            poder da internet através de cursos digitais. 
+            </p>
+            <span>
+              <p>
+            Não reivindicamos ou declaramos que ao usar o método, você ganhará
+            dinheiro ou recuperará seu dinheiro.
+            </p>
             </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+            <span>
+              <p>
+             Os depoimentos mostrados são negócios e vão variar com base no seu
+            esforço, no conhecimento que você adquirir e nas forças do mercado que estão além do controle de qualquer
+            pessoa.
+            </p>
+            </span>
+          
+        </span>
+      </div>
+
+      <div className='flex'>
+        <footer className='flex justify-start items-start ml-5'>Políticas de privacidade</footer>
+        <footer className='flex justify-end items-end ml-auto pr-6'>Políticas de privacidade</footer>
       </div>
     </main>
   )
